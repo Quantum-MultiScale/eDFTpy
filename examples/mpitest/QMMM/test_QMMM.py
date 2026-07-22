1050005ed6fb8a26e2195b8adee17708a694401e
jezsmartinez <jam1042@scarletmail.rutgers.edu>
2026-07-22 15:07:10 -0400

    Reduce the size of the cell to have a quick test for QM/MM

--- examples/mpitest/QMMM/test_QMMM.py
+++ examples/mpitest/QMMM/test_QMMM.py
@@ -5,6 +5,7 @@ from edftpy.api.parse_config import config2optimizer
 from edftpy.config import read_conf
 
 def test_QMMM():
+    '''Original TEST in 20 \AA cell size for testing reducing it to 5 \AA'''
     fname = 'qmmm.in'
     config = read_conf(fname)
     graphtopo = conf2init(config, parallel = True)
@@ -13,7 +14,8 @@ def test_QMMM():
     optimizer.optimize()
     print('Energy: ',optimizer.energy)
 #    graphtopo.assert_check(np.isclose(optimizer.energy, -17.34156, atol = 1E-3))
-    graphtopo.assert_check(np.isclose(optimizer.energy, -17.338300521208282, atol = 1E-3))
+#    graphtopo.assert_check(np.isclose(optimizer.energy, -17.338300521208282, atol = 1E-3)) # 20 \AA
+    graphtopo.assert_check(np.isclose(optimizer.energy, -12.164558943203993, atol = 1E-3)) # 5 \AA
 
 
 if __name__ == "__main__":
